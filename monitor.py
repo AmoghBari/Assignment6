482f825d937032db5b07eea5e410643e06801605
Sigmoid <sigmoid@sigmoid-20JVA03PIG>
2023-11-17 13:41:33 +0530

    Change

--- monitor.py
+++ monitor.py
@@ -1,18 +1,18 @@
-# import subprocess
-# import time
+import subprocess
+import time
 
-# Define the kubectl command
-# kubectl_command = "kubectl top pod"
+#Define the kubectl command
+kubectl_command = "kubectl top pod"
 
 print("Hello World")
 # Run the kubectl command and capture the output
 
-# try:
-#     while True:
-#         output = subprocess.check_output(kubectl_command, shell=True)
-#         # Decode the byte output to a string (assuming UTF-8 encoding)
-#         output_str = output.decode("utf-8")
-#         print(output_str)
-#         time.sleep(10)
-# except subprocess.CalledProcessError as e:
-#     print(f"Error running kubectl command: {e}")
+try:
+    while True:
+        output = subprocess.check_output(kubectl_command, shell=True)
+        # Decode the byte output to a string (assuming UTF-8 encoding)
+        output_str = output.decode("utf-8")
+        print(output_str)
+        time.sleep(10)
+except subprocess.CalledProcessError as e:
+    print(f"Error running kubectl command: {e}")
