6bb73ea0ca4bae7ca567a866ad5ea9a62b5770f8
AmoghBari <145555795+AmoghBari@users.noreply.github.com>
2023-12-12 14:48:06 +0530

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,18 +1,12 @@
-import subprocess
-import time
+import time 
+import psutil
 
-#Define the kubectl command
-kubectl_command = "kubectl top pod"
+def info():
+    cpu_percent = psutil.cpu_percent()
+    mem_info = psutil.virtual_memory()
+    return f"The cpu percentage: {cpu_percent}% and Memory percentage: {mem_info.percent}%"
 
-print("Hello World")
-# Run the kubectl command and capture the output
-
-try:
-    while True:
-        output = subprocess.check_output(kubectl_command, shell=True)
-        # Decode the byte output to a string (assuming UTF-8 encoding)
-        output_str = output.decode("utf-8")
-        print(output_str)
-        time.sleep(10)
-except subprocess.CalledProcessError as e:
-    print(f"Error running kubectl command: {e}")
+print("Hello World!")
+while True:
+    print(info())
+    time.sleep(10)
